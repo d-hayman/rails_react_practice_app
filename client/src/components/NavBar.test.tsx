6d8162a80e5f39420d6c087cbbb2eb4140e006da
dhayman <dhayman117@gmail.com>
2024-01-21 00:49:32 -0500

    normalize back to tutorial code

--- client/src/components/NavBar.test.tsx
+++ client/src/components/NavBar.test.tsx
@@ -1,26 +1,17 @@
 import { describe } from "node:test";
 import NavBar from "./NavBar";
-import { createRoot } from "react-dom/client";
 import { render, screen } from '@testing-library/react'
 import { MemoryRouter } from "react-router-dom";
 
-let container : any;
-
-beforeEach(() => {
-  container = document.createElement('div');
-  document.body.appendChild(container);
-});
-
-afterEach(() => {
-  document.body.removeChild(container);
-  container = null;
-});
-
-describe('can render and update a counter', () => {
+describe('NavBar component', () => {
+  const renderNavBar = () => {
+    render(<NavBar />, { wrapper: MemoryRouter });
+  };
   // Test first render and componentDidMount
   test("render the nav bar", () => {
-    render(<NavBar />, {wrapper: MemoryRouter});
+    renderNavBar();
 
     expect(screen.getByText("Posts List")).toBeInTheDocument();
+    expect(screen.getByText("New Post")).toBeInTheDocument();
   });
 });
