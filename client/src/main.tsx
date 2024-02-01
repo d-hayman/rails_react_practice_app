71787fe0cec224a73db8ff1bab892ce014980f34
dhayman <dhayman117@gmail.com>
2024-02-01 02:56:46 -0500

    Improved the look

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,6 +4,8 @@ import App from './App.tsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+document.getElementsByTagName("html")[0].setAttribute("data-bs-theme","dark");
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
