8bedcc84fb3aa55644f24507a65838ce26b430bd
dhayman <dhayman117@gmail.com>
2024-02-02 21:15:56 -0500

    Clean up routes

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,13 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      get 'search/articles'
-    end
-  end
-	root "articles#index"
-  resources :articles do
-    resources :comments
-  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -16,11 +7,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  root to: proc { [404, {}, ["Not found."]] }
+
+  #Dev routes - leave disabled
+  #root "articles#index"
+  #resources :articles do
+  #  resources :comments
+  #end
+
   #API routes
   namespace :api do
     namespace :v1 do
       resources :articles, only: [:index, :show, :create, :update, :destroy]
+      get 'search/articles'
       resources :auth, only: [:create, :destroy]
+      match "auth", to: "auth#destroy", via: "delete", defaults: { id: nil }
     end
   end
 end
